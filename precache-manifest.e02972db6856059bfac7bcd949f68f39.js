eb3154380fd7d78328af06b170705c23bbd33668
thu <thai.k.thu2000@gmail.com>
2019-08-13 20:58:34 +0700

    Updates

---
+++ precache-manifest.e02972db6856059bfac7bcd949f68f39.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c9bb6ad7c454214c784305d7a93a4a",
+    "url": "/MyWeb/index.html"
+  },
+  {
+    "revision": "8287bb8f84ab6929b0d7",
+    "url": "/MyWeb/static/css/2.14686bf5.chunk.css"
+  },
+  {
+    "revision": "0d3f0bec8dcb49ac4d2d",
+    "url": "/MyWeb/static/css/main.59f647e5.chunk.css"
+  },
+  {
+    "revision": "8287bb8f84ab6929b0d7",
+    "url": "/MyWeb/static/js/2.4b8f9fcd.chunk.js"
+  },
+  {
+    "revision": "0d3f0bec8dcb49ac4d2d",
+    "url": "/MyWeb/static/js/main.4d6ff8e5.chunk.js"
+  },
+  {
+    "revision": "61f12262916e16145a07",
+    "url": "/MyWeb/static/js/runtime~main.44b9dba7.js"
+  },
+  {
+    "revision": "ca6d938b65aeb25a7767c338e5a2d3ec",
+    "url": "/MyWeb/static/media/Alaster.ca6d938b.png"
+  },
+  {
+    "revision": "250e0b0c032ceafed51ec668a525e04a",
+    "url": "/MyWeb/static/media/Dante.250e0b0c.jpg"
+  },
+  {
+    "revision": "a3dfd7d0a34cbaedef90ca11721f3911",
+    "url": "/MyWeb/static/media/Dragon.a3dfd7d0.jpg"
+  },
+  {
+    "revision": "1d04a31701951ff4132e31e64bc3e684",
+    "url": "/MyWeb/static/media/MichealMyers.1d04a317.png"
+  },
+  {
+    "revision": "a8489c7c62e103a8685995fcd649a41e",
+    "url": "/MyWeb/static/media/NeroAndKyrie.a8489c7c.png"
+  },
+  {
+    "revision": "085aa93e76720bd2c3954d3c4983fff7",
+    "url": "/MyWeb/static/media/NeroScreenmate.085aa93e.mp4"
+  },
+  {
+    "revision": "aafc6c01a1d6d27ec99ef0fad0d8b3f6",
+    "url": "/MyWeb/static/media/OutOfHell.aafc6c01.mp4"
+  },
+  {
+    "revision": "e217024a3c2d5a70f09bb22a7a86f79b",
+    "url": "/MyWeb/static/media/OutOfHell.e217024a.png"
+  },
+  {
+    "revision": "ee12f10a54fd012f2f64e6092f4a8143",
+    "url": "/MyWeb/static/media/Snakes.ee12f10a.jpg"
+  },
+  {
+    "revision": "19e969c7403899b674fe61412a47b1ff",
+    "url": "/MyWeb/static/media/arrowIcon.19e969c7.png"
+  },
+  {
+    "revision": "95d3c918dea53557d28307827a578151",
+    "url": "/MyWeb/static/media/artIcon.95d3c918.png"
+  },
+  {
+    "revision": "54c0adcfd818a665b2663b28bcb966f3",
+    "url": "/MyWeb/static/media/background.54c0adcf.png"
+  },
+  {
+    "revision": "77d47d6e110ceb9612d9aaffabd6f4e5",
+    "url": "/MyWeb/static/media/codingIcon.77d47d6e.png"
+  },
+  {
+    "revision": "0d459f9d1e8ed510874e872da500a179",
+    "url": "/MyWeb/static/media/deviantartIcon.0d459f9d.png"
+  },
+  {
+    "revision": "b2c68574084bc3e10cd149cb4995e8ea",
+    "url": "/MyWeb/static/media/hamburgerIcon.b2c68574.png"
+  },
+  {
+    "revision": "a4c33368eaad2773ee9213b4699f8f93",
+    "url": "/MyWeb/static/media/linkedinIcon.a4c33368.png"
+  },
+  {
+    "revision": "32f96acb42e73a253ce000b63c22a976",
+    "url": "/MyWeb/static/media/myAvatar.32f96acb.jpg"
+  }
+]);
--- src/images/work/ImgWorkComp.js
+++
@@ -1,8 +0,0 @@
-import NeroScreenmateThumbnail from "./thumbnail/NeroScreenmate.gif"
-import OutOfHellThumbnail from "./thumbnail/OutOfHell.png"
-import NeroScreenmate from "./video/NeroScreenmate.mp4"
-import OutOfHell from "./video/OutOfHell.mp4"
-
-const ImgWorkComp = {NeroScreenmateThumbnail,OutOfHellThumbnail,NeroScreenmate,OutOfHell}
-
-export default ImgWorkComp; 
