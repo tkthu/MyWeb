762d86aa3b0d944dde97e5f3113cf01a6a212358
thu <thai.k.thu2000@gmail.com>
2019-08-13 18:57:16 +0700

    sixth commit

--- src/HomePage/WorkIcon.js
+++ src/HomePage/WorkIcon.js
@@ -7,7 +7,7 @@ import ImgComp from "../images/ImgComp.js";
 const WorkIcon = props => (	
 	<Link className="WorkIcon"
 		style={{margin:"20px"}} 
-		to={`/${props.toPage}`}
+		to={`MyWeb/${props.toPage}`}
 	>
 		<img src={ImgComp[props.imgSrc]} />
 	</Link>	
