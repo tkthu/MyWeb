eb203fa54bd6e6d64200b641bae53805d9ca7bdf
thu <thai.k.thu2000@gmail.com>
2019-12-27 15:26:36 +0700

    add another work

--- src/Components/images/work/ImgWorkComp.js
+++ src/Components/images/work/ImgWorkComp.js
@@ -1,8 +1,10 @@
 import NeroScreenmateThumbnail from "./thumbnail/NeroScreenmate.gif"
 import OutOfHellThumbnail from "./thumbnail/OutOfHell.png"
+import RectangleThumbnail from "./thumbnail/Rectangle.png"
 import NeroScreenmate from "./video/NeroScreenmate.mp4"
 import OutOfHell from "./video/OutOfHell.mp4"
+import Rectangle from "./video/Rectangle.mp4"
 
-const ImgWorkComp = {NeroScreenmateThumbnail,OutOfHellThumbnail,NeroScreenmate,OutOfHell}
+const ImgWorkComp = {NeroScreenmateThumbnail,OutOfHellThumbnail,RectangleThumbnail,NeroScreenmate,OutOfHell,Rectangle}
 
 export default ImgWorkComp; 
